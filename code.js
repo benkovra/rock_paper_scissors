5325ede477d03d754ea62147e9ecab2e8bee5658
benkovra <112646826+benkovra@users.noreply.github.com>
2022-11-19 19:03:27 -0500

    setting variables

--- code.js
+++ code.js
@@ -1,11 +1,40 @@
+function game(x){
+  sessionStorage.playerChoise=x;
 
-/*sets rock as 1, paper as 2 and scissors as 3*/
-if (y == 1) {
-    sessionStorage.opponentChoice = "rock";
+  var x=math.ceil(math.random()*3);
+
+    /*sets rock as 1, paper as 2 and scissors as 3*/
+  if (y == 1) {
+      sessionStorage.opponentChoice = "rock"; /*sessionStorage stores memory in the browzer*/
   }
   else if (y == 2) {
-    sessionStorage.opponentChoice = "paper";
+      sessionStorage.opponentChoice = "paper";
   }
   else if (y == 3) {
-    sessionStorage.opponentChoice = "scissors";
+      sessionStorage.opponentChoice = "scissors";
   }
+
+  if ((sessionStorage.playerChoise == "rock") && (sessionStorage.opponentChoice == "paper")){
+    sessionStorage.result = "lose";
+  }
+  else if ((sessionStorage.playerChoise == "scissors") && (sessionStorage.opponentChoice == "rock")){
+    sessionStorage.result = "lose";
+  }
+  else if ((sessionStorage.playerChoise == "paper") && (sessionStorage.opponentChoice == "scissors")){
+    sessionStorage.result = "lose";
+  }
+  else if ((sessionStorage.playerChoise == "paper") && (sessionStorage.opponentChoice == "rock")){
+    sessionStorage.result = "win";
+  }
+  else if ((sessionStorage.playerChoise == "rock") && (sessionStorage.opponentChoice == "scissors")){
+    sessionStorage.result = "win";
+  }
+  else if ((sessionStorage.playerChoise == "scissors") && (sessionStorage.opponentChoice == "paper")){
+    sessionStorage.result = "win";
+  }
+  
+  if (sessionStorage.playerChoice == sessionStorage.opponentChoice) {
+    sessionStorage.result = "tie";
+  }
+
+}
