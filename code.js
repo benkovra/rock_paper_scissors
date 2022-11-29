5c2eafdac7e286e1e9d4d4b48737f9259a23a889
benkovra <112646826+benkovra@users.noreply.github.com>
2022-11-29 11:47:34 -0500

    rock paper scissors works now! yay

--- code.js
+++ code.js
@@ -1,40 +1,53 @@
 function game(x){
-  sessionStorage.playerChoise=x;
-
-  var x=math.ceil(math.random()*3);
+  sessionStorage.playerChoice=x;
+  let image = document.getElementById('o-img');
+  var y=Math.ceil(Math.random()*3);
 
     /*sets rock as 1, paper as 2 and scissors as 3*/
   if (y == 1) {
       sessionStorage.opponentChoice = "rock"; /*sessionStorage stores memory in the browzer*/
+      image.src = "svg/rock.svg";
   }
   else if (y == 2) {
       sessionStorage.opponentChoice = "paper";
+      image.src = "svg/paper.svg";
   }
   else if (y == 3) {
       sessionStorage.opponentChoice = "scissors";
+      image.src = "svg/scissors.svg";
   }
 
-  if ((sessionStorage.playerChoise == "rock") && (sessionStorage.opponentChoice == "paper")){
+  if ((sessionStorage.playerChoice == "rock") && (sessionStorage.opponentChoice == "paper")){
     sessionStorage.result = "lose";
   }
-  else if ((sessionStorage.playerChoise == "scissors") && (sessionStorage.opponentChoice == "rock")){
+  else if ((sessionStorage.playerChoice == "scissors") && (sessionStorage.opponentChoice == "rock")){
     sessionStorage.result = "lose";
   }
-  else if ((sessionStorage.playerChoise == "paper") && (sessionStorage.opponentChoice == "scissors")){
+  else if ((sessionStorage.playerChoice == "paper") && (sessionStorage.opponentChoice == "scissors")){
     sessionStorage.result = "lose";
   }
-  else if ((sessionStorage.playerChoise == "paper") && (sessionStorage.opponentChoice == "rock")){
+  else if ((sessionStorage.playerChoice == "paper") && (sessionStorage.opponentChoice == "rock")){
     sessionStorage.result = "win";
   }
-  else if ((sessionStorage.playerChoise == "rock") && (sessionStorage.opponentChoice == "scissors")){
+  else if ((sessionStorage.playerChoice == "rock") && (sessionStorage.opponentChoice == "scissors")){
     sessionStorage.result = "win";
   }
-  else if ((sessionStorage.playerChoise == "scissors") && (sessionStorage.opponentChoice == "paper")){
+  else if ((sessionStorage.playerChoice == "scissors") && (sessionStorage.opponentChoice == "paper")){
     sessionStorage.result = "win";
   }
   
   if (sessionStorage.playerChoice == sessionStorage.opponentChoice) {
     sessionStorage.result = "tie";
+  }
+
+  if (sessionStorage.result == "win") {
+    document.getElementById('result').innerHTML="you win";
+  }
+  else if (sessionStorage.result == "lose") {
+    document.getElementById('result').innerHTML="you lose";
+  }
+  else if (sessionStorage.result == "tie") {
+    document.getElementById('result').innerHTML="you tie";
   }
 
 }
